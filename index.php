3735bbde2ebc00a2c27cc3be4b874521cd690dda
umpyl <566015@edu.rocmn.nl>
2023-04-04 13:43:40 +0200

    styling

--- index.php
+++ index.php
@@ -4,14 +4,38 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
     <link rel="stylesheet" href="style.css">
-    <title>VakantieWoningen</title>
 </head>
+
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section class="hero">
+            <h1>Welcome to Our Website</h1>
+            <p>We are a company that specializes in providing high-quality products and services to our customers.</p>
+            <button>Learn More</button>
+        </section>
+
+        <section class="services">
+            <h2>Our Services</h2>
+            <p>Here are some of the services that we offer:</p>
+        </section>
+    </main>
 </body>
+
 </html>
