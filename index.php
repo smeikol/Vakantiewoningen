63b952af14233ea01c5577dc0fb654daecb6b7f4
Rayanely <562891@edu.rocmn.nl>
2023-04-05 10:37:34 +0200

    Contact, Overicht en Styles Toegevoegd/Aangepast

--- index.php
+++ index.php
@@ -13,29 +13,47 @@ session_start();
 </head>
 
 <body>
-    <header>
+    <header id="navbar">
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Overzicht</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
     </header>
 
     <main>
-        <section class="hero">
-            <h1>Welcome to Our Website</h1>
-            <p>We are a company that specializes in providing high-quality products and services to our customers.</p>
-            <button>Learn More</button>
-        </section>
+        <div class="content">
+            <section class="hero">
+                <h1>Welcome to Our Website</h1>
+                <p>We are a company that specializes in providing high-quality products and services to our customers.</p>
+                <button>Learn More</button>
+            </section>
 
-        <section class="services">
-            <h2>Our Services</h2>
-            <p>Here are some of the services that we offer:</p>
-        </section>
+            <section class="services">
+                <h2>Our Services</h2>
+                <p>Here are some of the services that we offer:</p>
+            </section>
+        </div>
     </main>
 </body>
+<script>
+    window.onscroll = function() {
+        myFunction()
+    };
+
+    let navbar = document.getElementById("navbar");
+    let sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+</script>
 
 </html>
