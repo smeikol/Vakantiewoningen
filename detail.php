c3dc0cafc0d5ae065c78337bd54aad79a6ecb9fd
smeikol <564500@edu.rocmn.nl>
2023-04-07 12:11:15 +0200

    detail + begin filter

---
+++ detail.php
@@ -0,0 +1,103 @@
+<?php
+include_once 'connection.php';
+
+
+
+$sql = "SELECT * FROM `vw_woningen` WHERE `woningnummer` = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $_GET['woning']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_array()) {
+    $html[0] = '<img src="' . $row["afbeelding1"] . '" height="200px" width="200px">';
+    $html[1] = '<img src="' . $row["afbeelding2"] . '" height="200px" width="200px">';
+    $html[2] = '<img src="' . $row["afbeelding3"] . '" height="200px" width="200px">';
+    $html[3] = '<img src="' . $row["afbeelding4"] . '" height="200px" width="200px">';
+    $html[4] = '<img src="' . $row["afbeelding5"] . '" height="200px" width="200px"> <br>';
+    $html[5] = '<div>' . $row['adres'] . '</div> <br>';
+    $html[6] = '<div>' . $row['postcode'] . '</div> <br>';
+    $html[7] = '<div>' . $row['plaats'] . '</div> <br>';
+    $html[8] = '<div>' . $row['prijs'] . '</div> <br>';
+    $html[9] = '<div>' . $row['omschrijving'] . '</div> <br>';
+    $html[10] = '<div>' . $row['verkocht'] . '</div> <br>';
+    $html[11] = '<div> liggingen zijn: </div> <br>';
+}
+$counter = 11;
+
+$sql2 = "SELECT liggingen_id FROM `vw_liggingen` WHERE `VW_woningen_woningnummer` = ?";
+$stmt2 = $conn->prepare($sql2);
+$stmt2->bind_param('i', $_GET['woning']);
+$stmt2->execute();
+$result2 = $stmt2->get_result();
+
+while ($row = $result2->fetch_array()) {
+    $sql3 = "SELECT ligging_omschrijving FROM `vw_liggingen_omschrijving` WHERE `id` = ?";
+    $stmt3 = $conn->prepare($sql3);
+    $stmt3->bind_param('i', $row['liggingen_id']);
+    $stmt3->execute();
+    $result3 = $stmt3->get_result();
+
+    while ($row2 = $result3->fetch_array()) {
+        $counter = $counter + 1;
+        $html[$counter] = '<div>' . $row2['ligging_omschrijving'] . '</div> <br>';
+    }
+}
+
+$counter = $counter + 1;
+$html[$counter] = '<div> eigenschappen zijn: </div> <br>';
+
+$sql4 = "SELECT VW_eigenschappen_id FROM `vw_eigenschappen` WHERE `VW_woningen_woningnummer` = ?";
+$stmt4 = $conn->prepare($sql4);
+$stmt4->bind_param('i', $_GET['woning']);
+$stmt4->execute();
+$result4 = $stmt4->get_result();
+
+while ($row = $result4->fetch_array()) {
+    $sql5 = "SELECT eigenschappen FROM `vw_eigenschappen_lijst` WHERE `id` = ?";
+    $stmt5 = $conn->prepare($sql5);
+    $stmt5->bind_param('i', $row['VW_eigenschappen_id']);
+    $stmt5->execute();
+    $result5 = $stmt5->get_result();
+
+    while ($row2 = $result5->fetch_array()) {
+        $counter = $counter + 1;
+        $html[$counter] = '<div>' . $row2['eigenschappen'] . '</div> <br>';
+    }
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>details</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header id="navbar">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="overzicht.php">Overzicht</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <?php foreach ($html as $row) {
+        echo $row;
+    }
+    ?>
+
+</body>
+
+</html>
--- overzicht.php
+++ overzicht.php
@@ -1,14 +1,33 @@
 <?php
 include_once 'connection.php';
 $counter = 0;
-$sql = "SELECT `adres`, `plaats`, `afbeelding1` FROM `vw_woningen`";
+$sql = "SELECT `woningnummer`, `adres`, `plaats`, `afbeelding1` FROM `vw_woningen`";
+
+if(isset($_GET['search'])) {
+    $sql = 'SELECT `woningnummer`, `adres`, `plaats`, `afbeelding1` 
+    FROM `vw_woningen` 
+    WHERE `adres` LIKE ? OR 
+    `postcode` LIKE ? OR 
+    `plaats` LIKE ? OR 
+    `prijs` LIKE ? OR 
+    `verkocht` LIKE ?';
+}
+
 $stmt = $conn->prepare($sql);
+
+if(isset($_GET['search'])) {
+    $s = '%' . $_GET['search'] . '%';
+    $stmt->bind_param('sssss', $s, $s, $s, $s, $s);
+}
+
 $stmt->execute();
 $result = $stmt->get_result();
-
+if ($result->num_rows == 0) { 
+    header('Location: overzicht.php');
+}
 
 while ($row = $result->fetch_array()) {
-    $html[$counter] = '<img src="' . $row["afbeelding1"] . '" height="200px" width="200px">';
+    $html[$counter] = '<img onclick="gotodetails(' . $row["woningnummer"] . ')" src="' . $row["afbeelding1"] . '" height="200px" width="200px">';
     $counter = $counter + 1;
 }
 
@@ -43,6 +62,12 @@ while ($row = $result->fetch_array()) {
         echo $row;
     }
     ?>
+
+    <script>
+    function gotodetails(getheader) {
+        window.location.replace("detail.php?woning=" + getheader);
+    } 
+    </script>
 
 </body>
 
